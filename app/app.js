201e26a378e4f21ad01a3b7877c41b364e16c1d6
Madhuranga Senadheera <lilan.maduranga@gmail.com>
2016-02-17 10:24:46 +0530

    Modified the nested views

--- app/app.js
+++ app/app.js
@@ -5,17 +5,32 @@ angular.module('myApp', ['ngResource','ui.router','Home.controllers','User.contr
 .config(function($stateProvider) {
  
   $stateProvider    
+    .state('header', {
+        abstract:true,  
+        templateUrl: 'templates/header.html',
+        controller:'HomeController'
+    }) 
     // setup an abstract state for the tabs directive
-    .state('login', {
+    .state('header.login', {
+        parent:'header',
         url: '/login', 
         templateUrl: 'templates/login.html',
         controller:'DeskTopLoginController'
     })
-    .state('home', {
+    .state('header.home', { 
+        parent:'header',
         url: '/home', 
-        templateUrl: 'templates/home.html',
-        controller:'HomeController'
-    }) 
+        templateUrl: 'templates/home.html' 
+    })
+    .state('header2', {
+        abstract:true,  
+        templateUrl: 'templates/header2.html' 
+    })
+    .state('header2.home', { 
+        parent:'header2',
+        url: '/home2', 
+        templateUrl: 'templates/home.html' 
+    })
 
 });
 
