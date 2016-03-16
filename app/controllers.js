2c402de71349fe639aa31c63dddc51472fd7b072
Madhuranga Senadheera <lilan.maduranga@gmail.com>
2016-03-16 11:39:57 +0530

    Update the controller changers

--- app/controllers.js
+++ app/controllers.js
@@ -8,12 +8,10 @@ angular.module('Home.controllers',[])
 var UserModule =  angular.module('User.controllers',['DesktopUsers.service']);
 //  user login details
 UserModule.controller('DeskTopLoginController', function($scope,DesktopUser)
-{
-	$scope.title = "Lgoin"; 
-
+{ 
     $scope.desktop_login = function (desktop_user)
     { 
-    	// DesktopUser.get();
+    	console.log('Login function');
     }
 
 })
--- app/services.js
+++ app/services.js
@@ -3,9 +3,11 @@ var DesktopUsersServices = angular.module('DesktopUsers.service', ['ngResource']
 
 // id is the parameter
 DesktopUsersServices.factory('DesktopUser', function ($resource) {
-    return $resource('http://ssss.com/members/:id', {}, {
-        get: { method: 'POST' ,params: {id: '@id'}},
-        post: { method: 'GET' ,params: {id: '@id'}},
+    return $resource('http://localhost/rest_api/members/:id', {}, {
+        get: { method: 'GET'},
+        query: { method: 'GET' ,params: {id: '@id'}},
+        update: { method: 'PUT' ,params: {id: '@id'}},
+        post: { method: 'POST' ,params: {id: '@id'}},
         delete: { method: 'DELETE' ,params: {id: '@id'}}
     });
 }); 
